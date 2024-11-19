3bd23d806a5f041e01646c28ad8c30387b511aa8
leonArch <leomeireles66@gmail.com>
2024-11-19 12:04:08 -0300

    Project created

---
+++ src/test/java/com/projetowebservicesspringjpa/udemy/ProjetoWebServicesComSpringBootEJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetowebservicesspringjpa.udemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesComSpringBootEJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
