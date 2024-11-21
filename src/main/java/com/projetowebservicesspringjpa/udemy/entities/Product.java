5cc7b4b2a7c8b70533104543e62b1b57de3d3769
leonArch <leomeireles66@gmail.com>
2024-11-21 14:21:58 -0300

    Many-tomany association with JoinTable

--- src/main/java/com/projetowebservicesspringjpa/udemy/entities/Product.java
+++ src/main/java/com/projetowebservicesspringjpa/udemy/entities/Product.java
@@ -12,6 +12,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -26,13 +29,15 @@ public class Product implements Serializable{
 	private Long id;
 	private String name;
 	
-	@Column
 	@Value("TEXT")
 	private String text;
 	private Double price;
 	private String imgUrl;
 	
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+		joinColumns = @JoinColumn(name = "product_id"), 
+		inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {}
