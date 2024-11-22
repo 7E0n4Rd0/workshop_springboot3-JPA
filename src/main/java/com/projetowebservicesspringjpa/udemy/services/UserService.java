801907ee73093fb8b5edb86cbf24c707c1f52a8b
leonArch <leomeireles66@gmail.com>
2024-11-22 16:01:20 -0300

    Exception handling - update

--- src/main/java/com/projetowebservicesspringjpa/udemy/services/UserService.java
+++ src/main/java/com/projetowebservicesspringjpa/udemy/services/UserService.java
@@ -12,6 +12,8 @@ import com.projetowebservicesspringjpa.udemy.repositories.UserRepository;
 import com.projetowebservicesspringjpa.udemy.services.exceptions.DatabaseException;
 import com.projetowebservicesspringjpa.udemy.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 	
 	private void updateData(User entity, User obj) {
