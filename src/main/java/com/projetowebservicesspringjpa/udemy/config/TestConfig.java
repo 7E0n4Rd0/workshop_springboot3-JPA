bfa779e0511bffbd26abe19a4765740a66a02150
leonArch <leomeireles66@gmail.com>
2024-11-21 21:20:56 -0300

    Payment, one-to-one association

--- src/main/java/com/projetowebservicesspringjpa/udemy/config/TestConfig.java
+++ src/main/java/com/projetowebservicesspringjpa/udemy/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetowebservicesspringjpa.udemy.entities.Category;
 import com.projetowebservicesspringjpa.udemy.entities.Order;
 import com.projetowebservicesspringjpa.udemy.entities.OrderItem;
+import com.projetowebservicesspringjpa.udemy.entities.Payment;
 import com.projetowebservicesspringjpa.udemy.entities.Product;
 import com.projetowebservicesspringjpa.udemy.entities.User;
 import com.projetowebservicesspringjpa.udemy.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
